8264dffdacbee3ec30be35936bef7700cdb547fd
AlexisFioreCode <79189631+AlexisFioreCode@users.noreply.github.com>
2021-05-06 12:22:11 +0200

    Update Cards.js

--- src/components/acceuil/cards/Cards.js
+++ src/components/acceuil/cards/Cards.js
@@ -3,8 +3,8 @@ function Cards(props) {
         <div className='col-4 my-3'>
             <div className="card text-center bg-dark">
                 <div className="card-body text-white">
-                    <h5 className="card-title">{props.cards}</h5>
-                    <p className="card-text">Découvrez les {props.cards} de la galaxie Star Wars</p>
+                    <h5 className="card-title">{props.card}</h5>
+                    <p className="card-text">Découvrez les {props.card} de la galaxie Star Wars</p>
                     <a href="#" className="btn btn-warning">Par ici !</a>
                 </div>      
             </div>
